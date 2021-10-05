9c8d9519566e7cbbe373010ae2b497ed525c90a6
ose Okogbo <ose.okogbo@gmail.com>
2021-10-05 14:23:43 -0500

    Created controllers.  Checking routes to fix save

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -15,9 +15,24 @@ module.exports = {
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
     },
+
+    create: (req, res) => {
+        db.Book.create(req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+
+    update: (req, res) => {
+        db.Book.findOneAndUpdate({ id: req.params.id}, req.body)
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err))
+    },
+
+
+
     remove: (req, res) => {
         db.Book
-            .findById({_id: req.params.id})
+            .findById(req.params.id)
             .then(dbModel => dbModel.remove())
             .then(dbModel => res.json(dbModel))
             .catch(err => res.status(422).json(err));
